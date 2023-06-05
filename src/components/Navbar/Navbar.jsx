ebad3cd34bb863f723e97c2d29ad4c559c7ffccd
Geraldo Gomes <contato.gneto@gmail.com>
2023-06-05 02:34:57 -0300

    project finally. Version for tests

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,14 @@
+import { useNavigate } from "react-router-dom"
 import { ContainerNavbar, ImageProfile } from "./Navbar.style"
 
 export default function Navbar({userImage}) {
+   const navigate = useNavigate()
    return (
       <ContainerNavbar data-test="header">
          <span>TrackIt</span>
          <ImageProfile
             data-test="avatar"
-            onClick={() => {
-               window.location.reload()
-            }}
+            onClick={() => navigate('/')}
             src={userImage}
          />
       </ContainerNavbar>
